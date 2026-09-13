4bf11e42df1c384a505f855a798e2d7e3307d46c
alcarba5-wq <alcarba5@gmail.com>
2026-09-13 09:10:47 -0600

    Integrate SSD1306 OLED display and optimize I2C setup
    Refactor I2C initialization and add OLED display functionality.

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from machine import I2C, Pin
 from ens160 import ENS160
 from ahtx0 import AHT20
+from ssd1306 import SSD1306_I2C
+
+
 import time
 
-i2c = I2C(
-    0,
-    sda=Pin(0),
-    scl=Pin(1),
-    freq=100000
-)
-
+i2c=I2C(0,scl=Pin(1),sda=Pin(0),freq=100000)
 ens160 = ENS160(i2c)
 aht21 = AHT20(i2c)
+
+WIDTH =128 
+HEIGHT= 64
+i2c=I2C(0,scl=Pin(1),sda=Pin(0),freq=200000)
+oled = SSD1306_I2C(WIDTH,HEIGHT,i2c)
+
 
 print("ENS160 ID:", hex(ens160.get_id()))
 
@@ -37,8 +40,20 @@ while True:
 
         print("Temp:", round(temperatura, 2), "°C")
         print("Hum :", round(humedad, 2), "%")
+        
+        oled.fill(0)
+        oled.text(f"Temp: {temperatura:.1f} C", 0, 0)
+        oled.text(f"Hum:  {humedad:.1f} %", 0, 9)
+        oled.text(f"AQI:  {aqi}", 0, 18)
+        oled.text(f"TVOC: {tvoc} ppb", 0, 27)
+        oled.text(f"eCO2: {eco2} ppm", 0, 36)
+        oled.text(f"Air:  {tvoc_rating}", 0, 45)  # Rating corto
+        oled.text(f"CO2:  {eco2_rating.split('-')[0].strip()}", 0, 54)
+        
+        oled.show()
+        
 
     except Exception as e:
         print("Error:", e)
 
-    time.sleep(5)
+    time.sleep(1)
